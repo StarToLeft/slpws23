bf2b955e01246e4beda396715c75977c78cfd42a
Anton Hagsér <anton.hagser@epsidel.se>
2023-03-09 11:37:20 +0100

    feat(CRUD): add initial registration and product crud endpoint

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
-# require 'sinatra'
-# require 'slim'
+require 'sinatra'
+require 'slim'
 require 'sqlite3'
+
+enable :sessions
 
 require_relative 'models/user'
 require_relative 'models/product'
@@ -34,14 +36,79 @@ user1 = User.find(user.id)
 puts user1.username
 
 # ? place a bid
-puts "Place bid: " + ProductManager.place_bid(user1, product1, 100).to_s
-puts "Place bid: " + ProductManager.place_bid(user1, product1, 100).to_s
-puts "Place bid: " + ProductManager.place_bid(user1, product1, 150).to_s
+puts 'Place bid: ' + ProductManager.place_bid(user1, product1, 100).to_s
+puts 'Place bid: ' + ProductManager.place_bid(user1, product1, 100).to_s
+puts 'Place bid: ' + ProductManager.place_bid(user1, product1, 150).to_s
 
 sleep(2)
 
-puts "Place bid: " + ProductManager.place_bid(user1, product1, 180).to_s
+puts 'Place bid: ' + ProductManager.place_bid(user1, product1, 180).to_s
 
-# get('/get') do
-#     slim(:home)
-# end
+puts 'Product id: ' + product1.id
+
+get('/get') do
+    slim(:home)
+end
+
+get('/profile/:username') do
+    user = User.find_by_username(params[:username])
+    slim(:profile, locals: { user: user })
+end
+
+get('/login') do
+    slim(:login)
+end
+
+post('/login') do
+    # TODO: add jwts (skip refresh tokens for now)
+
+    user = User.find_by_username(params[:username])
+    if user && Auth.authenticate(user, params[:password])
+        session[:user_id] = user.id
+        # TODO: replace user_id with token in session (json-web-token)
+
+        redirect('/')
+    else
+        slim(:login)
+    end
+end
+
+get('/logout') do
+    session[:user_id] = nil
+    redirect('/')
+end
+
+get('/register') do
+    slim(:register)
+end
+
+post('/register') do
+    # TODO: Password requirements
+    # Todo: Email check
+    # Todo: Username check
+    # Todo: introduce type checks
+    # Todo: check if account already exists
+
+    user = User.new(params[:username], Auth.encrypt_password(params[:password]), nil, Time.now, params[:email])
+    user.insert
+    redirect('/')
+end
+
+# Product
+get('/product/:product_id') do
+    product = Product.find(params[:product_id])
+    puts product.inspect
+    slim(:product, locals: { product: product })
+end
+
+post('/product') do
+    # TODO: introduce type checks
+
+    user = User.find(session[:user_id])
+    creation_date = Time.now
+    expiration_date = creation_date + (5 * 24 * 60 * 60)
+    product = Product.new(user.id, params[:title], params[:description], creation_date, expiration_date, false, nil,
+                          nil)
+    product.insert
+    redirect('/')
+end
